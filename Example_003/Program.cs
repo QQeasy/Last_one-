14a207e8588f89b4efb80a401879b3c9a90f23f9
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-18 23:06:18 +0300

    2 commit

---
+++ Example_003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число А");
+int numA = new Random().Next(1, 10);
+Console.WriteLine(numA);
+Console.WriteLine("Введите второе число Б");
+int numB = new Random().Next(1, 10);
+Console.WriteLine(numB);
+int summ1 = numA + numB;
+Console.WriteLine("Сумма числа А и Б");
+Console.WriteLine(summ1);
---
+++ Example_004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("Привет, "); Console.Write(username);
+}
+else
+{
+    Console.Write("Пока, "); Console.Write(username);
+}
