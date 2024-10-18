093912345d50a46a395b5746406c1d329dd881d4
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-18 23:56:18 +0300

    первый семинар

---
+++ Example_seminar_001/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+string num1 = Console.ReadLine();
+int numA = Convert.ToInt32(num1);
+Console.WriteLine("Введите второе число: ");
+string num2 = Console.ReadLine();
+int numB = Convert.ToInt32(num2);
+  
+if (numA*numA == numB)
+{
+    Console.WriteLine("Число " + numB + " является квадратом числа " + numA);
+}
+else
+{
+    Console.WriteLine("Число " + numB + " не является квадратом числа " + numA);
+}
+   
