3a0feca79773dc35121548c97ec67cf76db91012
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-18 23:40:52 +0300

    3 commit

---
+++ Example_005/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+int a = 1;
+int b = 21;
+int c = 135;
+int d = 16;
+int e = 17777;
+int max = a;
+if (a > max)
+{
+    max = a;
+}
+
+if (b > max)
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+if (d > max)
+{
+    max = d;
+}
+
+if (e > max)
+{
+    max = e;
+}
+
+Console.Write("max = ");
+Console.Write(max);
