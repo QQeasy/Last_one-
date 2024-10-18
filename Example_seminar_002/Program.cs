408d5e8a5fa9307d533dc43457732d07cd70ab56
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-19 00:08:52 +0300

    2  задачка с первого семинара

---
+++ Example_seminar_002/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели:");
+string week = Console.ReadLine();
+int num = Convert.ToInt32(week);
+if (num == 1)
+{
+    Console.WriteLine(num + "-й день недели - это понедельник!");
+}
+if (num == 2)
+{
+    Console.WriteLine(num + "-й день недели - это вторник!");
+}
+if (num == 3)
+{
+    Console.WriteLine(num + "-й день недели - это среда!");
+}
+if (num == 4)
+{
+    Console.WriteLine(num + "-й день недели - это четверг!");
+}
+if (num == 5)
+{
+    Console.WriteLine(num + "-й день недели - это пятница!");
+}
+if (num == 6)
+{
+    Console.WriteLine(num + "-й день недели - это суббота!");
+}
+if (num == 7)
+{
+    Console.WriteLine(num + "-й день недели - это воскресенье!");
+}
+if (num > 7)
+{ 
+    Console.WriteLine(num + " - вы ввели неверное число, оно не соответсвтует диапазону (1:7)");
+}
