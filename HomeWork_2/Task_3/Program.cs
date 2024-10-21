b13f4f78a5255c19aad70ea51ad55cfc88b74642
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-21 22:22:32 +0300

    доделать!!! 21.10.24

---
+++ HomeWork_2/Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+
+int RandomInt(string massage)
+{
+    int i = 0;
+    int num = Convert.ToInt32(Console.ReadLine());
+    int [] arrey = new int[num];
+    while ( i < num)
+    {
+        arrey[i] = new Random().Next(1, 1000);
+        i++;
+    }
+    return arrey[1];
+    massage = Console.Write("Введите к-во цифр для сравнения");
+}
+
+RandomInt();
