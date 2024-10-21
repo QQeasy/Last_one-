b0f2d33d27fd5b8578cb7f8ad5150466a3974e7a
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-21 20:55:41 +0300

    seminar 2 1 zadacha

---
+++ Example_seminar_011/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int min = 10;
+int max = 99; 
+
+void MaxDid(int a, int b)
+{
+    int ramdomNum = new Random().Next(a, b+1);
+    a = ramdomNum/10;
+    b = ramdomNum%10;
+    int max = 0;
+    if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+Console.WriteLine(ramdomNum + " -> " + max);
+}
+MaxDid(min,max);
+
+
