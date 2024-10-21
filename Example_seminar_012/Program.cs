ddb68db6459bb5211af30c8b9833609c5574b180
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-21 21:34:12 +0300

    решил задачу 012

---
+++ Example_seminar_012/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int Num1 = Convert.ToInt32(Console.ReadLine());
+int Num2 = Convert.ToInt32(Console.ReadLine());
+
+void sqrt2(int a, int b)
+{
+   
+    if (a*a == b)
+    {
+       Console.WriteLine("ДА"); 
+    }
+    else
+    {
+       Console.WriteLine("НЕТ");
+    }
+}
+
+sqrt2(Num1, Num2);
+
+
+
