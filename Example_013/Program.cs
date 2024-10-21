6bd707c699d5464ffe49cf574f1b8e26dbd14659
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-21 21:08:45 +0300

    2 zadacha 2 seminar

---
+++ Example_013/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int pos = 2;
+// a - позиция числа
+void MaxDid(int a)
+{
+    int ramdomNum = new Random().Next(100, 1000);
+    string num1 = Convert.ToString(ramdomNum/100);
+    string num2 = Convert.ToString(ramdomNum%10);
+    int num = Convert.ToInt32(num1 + num2);
+Console.WriteLine(ramdomNum + " --> " + num);
+
+}
+
+MaxDid(pos);
+
+
+
