10caa541e7830b2a4eaee9682d59d10a77947fac
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-21 19:57:54 +0300

    второй урок первый пример

---
+++ Example_011/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Clear();
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 113;
+int a2 = 3;
+int a3 = 1;
+
+Console.WriteLine(Max(a1, a2, a3));
