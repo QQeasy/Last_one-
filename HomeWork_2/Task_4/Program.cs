1735e0bdf4ed10202fe285bc860274131cee9996
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-22 21:47:10 +0300

    решил 3 задачу 2 дз

---
+++ HomeWork_2/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+int num = ReadNum("Введите число");
+if (num%2==0)
+{
+    Console.WriteLine("Число " + num + " является четным!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " является нечетным!");
+}
+
+// ввод числа int
+int ReadNum(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
