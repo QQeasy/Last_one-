ba169ed6bb50ab2f9de5a61432305a48eaab3de9
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-22 21:58:35 +0300

    решил последнюю задачу 2 дз

---
+++ HomeWork_2/Task_3/Task_5/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+int ReadNum(string massage)
+{
+    Console.WriteLine(massage);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+void WriteCHET(int num)
+{
+    int i = 1;
+    while (i < num + 1)
+    {
+        if (i%2==0)
+        {
+            Console.Write(i + " ");
+        }
+    i++;
+    }
+}
+int num1 = ReadNum("Введите количество чисел N:");
+WriteCHET(num1);
