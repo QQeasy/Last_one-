c7a67468a97bdfbfd1f7623ab9c762265be83c6b
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-26 12:20:28 +0300

    2323'

---
+++ HomeWork_4/Task_23/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число N: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+void sqrt(int N)
+{
+    for (int i = 1; i < N+1; i++)
+    {
+        Console.Write(i*i*i + " ");
+    }
+}
+sqrt(n1);
