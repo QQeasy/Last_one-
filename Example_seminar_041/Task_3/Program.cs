0d18d65a1f64cfe05a439a40db71da4ba5ca7952
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-26 15:13:17 +0300

    4 seminar

---
+++ Example_seminar_041/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 28: Напишите программу, которая принимает на вход 
+//число N и выдаёт произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+
+Console.Write($"Введите число чисел N: ");
+int a = ReadInt();
+int mult = 1;
+for (int i = 1; i < a+1; i++)
+{
+    mult *= i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {a} = {mult}");
+
+
+
+
+
+int ReadInt()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
