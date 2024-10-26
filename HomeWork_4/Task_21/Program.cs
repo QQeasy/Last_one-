34003c08cdea180a5d6307e480eb63177b8ca158
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-26 12:12:57 +0300

    2 zadaxa

---
+++ HomeWork_4/Task_21/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координаты точки А");
+Console.Write("Координата x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата у1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Координата x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата у2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Координата z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+int count = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1);
+double summ = Math.Sqrt(count);
+Console.WriteLine(summ);
