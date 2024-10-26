1e2f147428ec05ea9689ee4587237d0127af18e5
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-26 15:42:14 +0300

    HW4

---
+++ HomeWork_5/Task_25/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.Write($"Введите количество элементов массива: ");
+
+
+int num1 = ReadInt();
+
+int[] numbers = new int[num1];
+
+FillArrey(numbers);
+
+//Методы 
+//Считывает число
+int ReadInt()
+{
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+//1 заполняет и выводит массив
+int[] FillArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = new Random().Next(0, 2);
+        Console.Write($"{arrey[i]} ");
+    }
+    return arrey;
+
+}
