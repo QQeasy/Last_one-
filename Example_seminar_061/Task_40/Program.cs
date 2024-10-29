767a003da9e3da4da0fde4e77964efec2eb4cead
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-29 21:45:06 +0300

    доделать

---
+++ Example_seminar_061/Task_40/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Clear();
+Console.WriteLine($"Введите два числа:");
+
+
+
+
+TrGipotenuza(Read2Num());
+
+
+
+void TrGipotenuza(int[] arrey)
+{   
+    if (arrey.Length > 3)
+    {
+    Console.WriteLine($"Вы ввели слишком много чисел! У треугольника три стороны!");
+    }
+    if (arrey[0] < arrey[1] + arrey[2] && 
+        arrey[1] < arrey[2] + arrey[0] && 
+        arrey[2] < arrey[0] + arrey[1])
+    {
+        Console.WriteLine($"Tреугольник c такими сторонами существует!");
+    }
+    else
+    {
+        Console.WriteLine($"Треугольника с такими сторонами быть не может!");
+    }
+}
+    
+
+
+
+int[] Read2Num()
+{
+    int[] storoni = new int[3];
+    for (int i = 0; i < storoni.Length; i++)
+    {
+        storoni[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return storoni;
+}
+
+void PrintArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write($"{arrey[i]} ");
+
+    }
+}
---
+++ Example_seminar_061/Task_42/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.Write($"Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+FillArrey(num1);
+
+void FillArrey(int num1)
+{
+    int n = num1;
+    while (num1 != 0)
+    {
+    num1 /= 2;
+    count++;
+    Console.WriteLine($"{num1}");
+    }
+
+    Console.WriteLine($"{count} - число элементов массива.");
+
+    int[] newArrey = new int[count];
+    for (int i = 0; i < newArrey.Length; i++)
+    {
+        newArrey[i] = n %= 2;
+        n /= 2;
+    }
+
+    for (int i = 0; i < newArrey.Length; i++)
+    {
+        Console.Write($"{newArrey[i]} ");
+
+    }
+}
