df7b7bc6cfcb25cc5557bf4e6c6969f1487b7acf
EGOR_QQ <oopyatkinoo@mail.ru>
2024-10-30 21:56:41 +0300

    решил семинарскую задачу

--- Example_seminar_061/Task_42/Program.cs
+++ Example_seminar_061/Task_42/Program.cs
@@ -2,31 +2,30 @@
 Console.Write($"Введите число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 int count = 0;
+int num2 = num1;
 
-FillArrey(num1);
-
-void FillArrey(int num1)
+while (num2 != 0)
 {
-    int n = num1;
-    while (num1 != 0)
-    {
-    num1 /= 2;
+    num2 /= 2;
     count++;
-    Console.WriteLine($"{num1}");
-    }
-
-    Console.WriteLine($"{count} - число элементов массива.");
-
-    int[] newArrey = new int[count];
-    for (int i = 0; i < newArrey.Length; i++)
-    {
-        newArrey[i] = n %= 2;
-        n /= 2;
-    }
-
-    for (int i = 0; i < newArrey.Length; i++)
-    {
-        Console.Write($"{newArrey[i]} ");
-
-    }
+    Console.WriteLine($"{num2}");
 }
+
+
+int num3 = num1;
+
+Console.WriteLine($"{count} - число элементов массива.");
+int[] newArrey = new int[count];
+for (int i = 1; i < newArrey.Length+1; i++)
+{
+
+    newArrey[newArrey.Length - i] = num1%2;
+    num1 /= 2;
+}
+Console.Write($"( ");
+for (int i = 0; i < newArrey.Length; i++)
+{
+    Console.Write($"{newArrey[i]} ");
+}
+Console.Write($")");
+Console.Write($" - десятичное представление числа {num3}");
